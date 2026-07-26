8f7c6a63f1f42319f1942efe3e03e9e90d2e4279
Umbrex <226073274+Umbrex47@users.noreply.github.com>
2026-07-26 17:35:37 -0500

    fix: class User

--- src/models/user.py
+++ src/models/user.py
@@ -1,12 +1,8 @@
 
 # Esto es la clase Usuario y devuelve un objeto tipo usuario con sus respectivos parametros
 class User :
-    def __init__(self, email, name, age) -> None:
+    def __init__(self, email, name, age, password) -> None:
             self.email = email
             self.age = age
             self.name = name
-
-
-nuevo_usuario = User("Alejandro@gmail.com",  "Alejandro", 12)
-
-print(nuevo_usuario.age)
+            self.password = password
